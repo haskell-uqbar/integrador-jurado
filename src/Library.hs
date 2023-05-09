2e48c7cd7e252a1530e92863d1120de3d8a0c0e1
Lucas Spigariol <lspigariol@gmail.com>
2023-05-09 13:33:52 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -6,13 +6,13 @@ doble numero = numero + numero
 
 type CorteSuprema = [Juez]
 type Juez = Ley -> Bool
-type Partido = String
+type Sector = String
 type Tema = String
 
 data Ley = UnaLey{
     tema :: Tema, 
     presupuesto :: Number, 
-    partidos :: [Partido]
+    sectores :: [Sector]
 } deriving (Show,Eq)
 
 agenda = ["Cannabis en Medicina", "Educacion Superior", "Tenis de Mesa", "Tenis"]
@@ -32,31 +32,34 @@ juezBasico ley = elem (tema ley) agenda
 juezPositivo _ = True
 
 juezInventado :: Juez
-juezInventado ley = juezDePartido "Universidad Tecnologica Nacional" ley && juezDePresupuesto 5 ley
+juezInventado ley = juezDeSector "Universidad Tecnologica Nacional" ley && juezDePresupuesto 5 ley
 
 juezUltimo = juezDePresupuesto 15
 
-supremaCorte = [juezBasico, juezDePartido "Sector Financiero", juezDePresupuesto 10, juezDePresupuesto 20, juezDePartido "Partido Conservador"]
+supremaCorte = [juezBasico, juezDeSector "Sector Financiero", juezDePresupuesto 10, juezDePresupuesto 20, c "Partido Conservador"]
 
 -- Funciones generales
 hayElementoEnComun lista1 lista2 = any (flip elem lista1) lista2
 hayElementoEnComun' lista1 lista2 = any (`elem` lista1) lista2
+hayElementoEnComun'' lista1 lista2 = any (\x -> elem x lista1) lista2
 
 substring string1 "" = False 
 substring palabra1 (letra2:palabra2) = palabra1 == (take (length palabra1) palabra2) || substring palabra1 palabra2 
 
-elPartidoApoyaLaLey partido ley = elem partido (partidos ley) 
-juezDePresupuesto numero = (<= numero).(presupuesto) 
-   
+elSectorApoyaLaLey partido ley = elem sector (sectores ley) 
+elSectorApoyaLaLey' partido = elem sector.sectores 
 
-juezDePartido = elPartidoApoyaLaLey   
+juezDePresupuesto numero ley = presupuesto ley <= numero
+juezDePresupuesto' numero = (<= numero).presupuesto 
+
+juezDeSector = elSectorApoyaLaLey   
 --------------------- Las Leyes
 -- 1
 sonCompatibles :: Ley -> Ley -> Bool
-sonCompatibles ley1 ley2 = (partidoEnComun ley1 ley2) && (mismosTemas ley1 ley2)
+sonCompatibles ley1 ley2 = sectoresEnComun ley1 ley2 && mismosTemas ley1 ley2
 
-partidoEnComun :: Ley -> Ley -> Bool
-partidoEnComun ley1 ley2 = hayElementoEnComun (partidos ley1) (partidos ley2)
+sectoresEnComun :: Ley -> Ley -> Bool
+sectoresEnComun ley1 ley2 = hayElementoEnComun (sectores ley1) (sectores ley2)
 
 mismosTemas :: Ley -> Ley -> Bool
 mismosTemas ley1 ley2 = substring (tema ley1) (tema ley2  ) || substring (tema ley2) (tema ley1)
@@ -71,10 +74,12 @@ mayoria jueces = div (length jueces) 2 + 1
 mayoria' jueces = length jueces / 2
 
 cuantosVotosPositivos ::  Ley -> CorteSuprema -> Number
-cuantosVotosPositivos  ley jueces = 
-    length (filter (\juez -> juez ley) jueces)
+cuantosVotosPositivos  ley jueces = length (filter (\juez -> juez ley) jueces)
 
-cuantosVotosPositivos' ley = length.filter ($ ley)
+cuantosVotosPositivos' ley = length.filter ($ ley) 
+cuantosVotosPositivos'' ley jueces = length (filter ($ ley) jueces)
+cuantosVotosPositivos''' ley jueces = length (filter (vota ley) jueces)
+vota ley juez = juez ley
 
 
 ahoraSiSonConstitucionales leyes corteSuprema nuevosJueces = 
@@ -96,23 +101,22 @@ leyesPorConstitucionalidad funcion corteSuprema leyes = filter ((funcion.(votoCo
 -------------------- Cuestion de principios
 
 -- Las más compactas
-borocotizar = map ((.) not)
-borocotizar' = map (not.)
+borocotizar = map (not.)
+borocotizar' jueces = map (not.) jueces
 -- Con lambda
-borocotizar'' = map (\juez -> (not.juez))
+borocotizar'' jueces = map (\juez -> (not.juez)) jueces
 -- Delegando
+borocotizar''' jueces = map borocotizarJuez jueces
+borocotizar'''' = map borocotizarJuez
+
 borocotizarJuez:: Juez -> Juez
 borocotizarJuez juez = not.juez
 
 borocotizarJuez':: Juez -> Ley -> Bool
 borocotizarJuez' juez ley = not (juez ley)
 
-
-borocotizar''' jueces = map borocotizarJuez jueces
-
---leyesQueVota juez = filter (\ley -> juez ley)
---leyesQueVota' juez = filter juez
-
 leyesQueVota = filter
+leyesQueVota' juez = filter (\ley -> juez ley)
+leyesQueVota'' juez = filter juez
 
-juezCoincideCon juez sector leyes = all (elPartidoApoyaLaLey sector) (leyesQueVota juez leyes)
+juezCoincideCon juez sector leyes = all (elSectorApoyaLaLey sector) (leyesQueVota juez leyes)
